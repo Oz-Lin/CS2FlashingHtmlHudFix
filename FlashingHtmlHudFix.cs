1dc102eca26617845b01b9c02297561d31a94668
M-archand <13469995+M-archand@users.noreply.github.com>
2024-11-18 09:07:07 -0500

    Fix folder structure/plugin name

---
+++ FlashingHtmlHudFix.cs
@@ -0,0 +1,45 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+
+namespace FlashingHtmlHudFix
+{
+    [MinimumApiVersion(247)]
+    public partial class FlashingHtmlHudFix : BasePlugin
+    {
+        public override string ModuleName => "FlashingHtmlHudFix";
+        public override string ModuleVersion => "1.0";
+        public override string ModuleAuthor => "Deana https://github.com/girlglock";
+
+        private CCSGameRules? _gameRules;
+
+        public override void Load(bool hotReload)
+        {
+            RegisterListener<Listeners.OnTick>(OnTick);
+            RegisterListener<Listeners.OnMapStart>(OnMapStartHandler);
+        }
+
+        private void OnMapStartHandler(string mapName)
+        {
+            _gameRules = null;
+        }
+
+        private void InitializeGameRules()
+        {
+            var gameRulesProxy = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").FirstOrDefault();
+            _gameRules = gameRulesProxy?.GameRules;
+        }
+
+        private void OnTick()
+        {
+            if (_gameRules == null)
+            {
+                InitializeGameRules();
+            }
+            else
+            {
+                _gameRules.GameRestart = _gameRules.RestartRoundTime < Server.CurrentTime;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 A CS2 CS# Plugin that uses a workaround found by Poggu to make .PrintToCenterHTML not flash every second by setting
 
 GameRestart = true if RestartRoundTime < Server.CurrentTime
+
+> [!IMPORTANT]
+> Install the plugin by putting the `FlashingHtmlHudFix` folder in `addons/counterstrikesharp/plugins`
